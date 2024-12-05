501440da67a279c71c55b5402830dca7fef1008b
inathan44 <inathan44@yahoo.com>
2024-12-05 01:34:48 -0500

    feat: implement auth middleware, protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,70 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { adminAuth } from '@/firebaseAdmin';
+import { NextRequest, NextResponse } from 'next/server';
+import { authMiddleware, redirectToLogin } from 'next-firebase-auth-edge';
+import { clientConfig, serverConfig } from '@/firebaseConfig';
+
+const PUBLIC_PATHS = ['/signup', '/login', '/'];
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value;
-  console.log('token:', token);
-
-  if (token) {
-    try {
-      await adminAuth.verifyIdToken(token);
-      const url = request.nextUrl.clone();
-
-      if (url.pathname === '/login' || url.pathname === '/signup') {
-        url.pathname = '/';
+  return authMiddleware(request, {
+    loginPath: '/api/login',
+    logoutPath: '/api/logout',
+    apiKey: clientConfig.apiKey,
+    cookieName: serverConfig.cookieName,
+    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+    cookieSerializeOptions: serverConfig.cookieSerializeOptions,
+    serviceAccount: serverConfig.serviceAccount,
+    handleValidToken: async (_, headers) => {
+      if (PUBLIC_PATHS.includes(request.nextUrl.pathname)) {
+        const url = request.nextUrl.clone();
+        url.pathname = '/groups';
         return NextResponse.redirect(url);
       }
-    } catch (error) {
-      console.error('Error verifying token:', error);
-    }
-  }
 
-  return NextResponse.next();
+      return NextResponse.next({
+        request: {
+          headers,
+        },
+      });
+    },
+    handleInvalidToken: async (reason) => {
+      console.info('Missing or malformed credentials', { reason });
+
+      const response = redirectToLogin(request, {
+        path: '/login',
+        publicPaths: PUBLIC_PATHS,
+      });
+
+      // Invalidate the cookie
+      response.cookies.set(serverConfig.cookieName, '', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 0,
+        path: '/',
+      });
+
+      return response;
+    },
+    handleError: async (error) => {
+      console.error('Unhandled authentication error', { error });
+
+      const response = redirectToLogin(request, {
+        path: '/login',
+        publicPaths: PUBLIC_PATHS,
+      });
+
+      // Invalidate the cookie
+      response.cookies.set(serverConfig.cookieName, '', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 0,
+        path: '/',
+      });
+
+      return response;
+    },
+  });
 }
 
 export const config = {
-  matcher: ['/login', '/signup'],
+  matcher: ['/', '/((?!_next|api|.*\\.).*)', '/api/login', '/api/logout'],
 };
