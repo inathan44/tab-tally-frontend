1a5620c841d50232d59199b78cd2956ddf0cdc79
inathan44 <inathan44@yahoo.com>
2024-12-05 23:36:26 -0500

    refactor: abstract client logic to separate component

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,17 +1,5 @@
 import ClientNavbar from '@/app/components/navbar/ClientNavbar';
-import { getTokens } from 'next-firebase-auth-edge';
-import { cookies } from 'next/headers';
-import { clientConfig, serverConfig } from '@/firebaseConfig';
 
 export default async function Navbar() {
-  const tokens = await getTokens(await cookies(), {
-    apiKey: clientConfig.apiKey,
-    cookieName: serverConfig.cookieName,
-    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
-    serviceAccount: serverConfig.serviceAccount,
-  });
-
-  const tokenValid = !!tokens;
-
-  return <ClientNavbar validToken={tokenValid} />;
+  return <ClientNavbar />;
 }
